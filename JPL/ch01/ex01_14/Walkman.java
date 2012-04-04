108d55291076bd7198c93f19ee4c5c0910430c3e
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2012-04-04 12:28:34 +0900

    add ex01_14

--- JPL/ch01/ex01_14/Walkman.java
+++ JPL/ch01/ex01_14/Walkman.java
@@ -1,7 +1,7 @@
 package ch01.ex01_14;
 
 class Walkman {
-  private static final int MAX_MUSIC_NUM = 1000;  
+  public static final int MAX_MUSIC_NUM = 1000;  
   private String[] musicSet; // 音楽セット
   private int nowPlayingIndex; /* 現在再生中の Music を表す(-1 - MAX_MUSIC_NUM-1)
                                 * -1 で停止を表す*/
@@ -12,8 +12,8 @@ class Walkman {
   }
 
   /** index で指定した音楽が削除できるか判定する */
-  private booleand canRemoveMusic(int index) {
-    if (index < 0 || index >= music.length) {
+  private boolean canRemoveMusic(int index) {
+    if (index < 0 || index >= musicSet.length) {
       return false;
     } else if (nowPlayingIndex == index) { // 指定した音楽が再生中
       return false;
@@ -23,7 +23,7 @@ class Walkman {
 
   /** music を音楽セットに追加する
     * 音楽セットがいっぱいの場合は -1 を返す */
-  public int addMusic(Strng music) {
+  public int addMusic(String music) {
     for (int i = 0; i < musicSet.length; ++i) {
       if (musicSet[i] == null) {
         musicSet[i] = music;
@@ -49,7 +49,7 @@ class Walkman {
 
   /** music で指定した曲を音楽セットから削除する
    *  削除した音楽を返す */  
-  public String[] removeMusic(String music) {
+  public String removeMusic(String music) {
     int index = -1;
     for (int i = 0; i < musicSet.length; ++i) {
       if (musicSet[i].equals(music)) {
@@ -62,14 +62,22 @@ class Walkman {
     return removeMusic(index);
   }
 
+  /** index で指定した音楽を返す */
+  public String getMusic(int index) {
+    if (index < 0 || index >= musicSet.length) {
+      return null;
+    }
+    return musicSet[index];
+  }
+
   /** 音楽セットを返す */  
   public String[] getMusicSet() {
     return musicSet;
   }
-
+  
   /** index で指定した音楽を再生する */
   public void playMusic(int index) {
-    if (index < 0 || index >= music.length) {
+    if (index < 0 || index >= musicSet.length) {
       System.out.println("Invalid index");
     } else if (musicSet[index] == null) {
       System.out.println("The music with index does not exit");
