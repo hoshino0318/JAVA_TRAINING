07456125bfcaf0ccd614ee4b3f976998eea39e1d
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2012-04-28 19:44:12 +0900

    add ex03_08

---
+++ JPL/ch03/ex03_08/PassengerVehicleTest.java
@@ -0,0 +1,43 @@
+package ch03.ex03_08;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PassengerVehicleTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void testConstructorSucceed() {
+    PassengerVehicle pV1 = new PassengerVehicle("Bob", 5, 1);
+    
+    assertEquals(pV1.getSeatNum(), 5);
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void testConstructorThrowException1() {
+    PassengerVehicle pV1 = new PassengerVehicle("Bob", 1, 5);
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void testConstructorThrowException2() {
+    PassengerVehicle pV1 = new PassengerVehicle("Bob", -1, 1);
+  }
+  
+  @Test (expected = IllegalArgumentException.class)
+  public void testConstructorThrowException3() {
+    PassengerVehicle pV1 = new PassengerVehicle("Bob", 1, -1);
+  }
+  
+  @Test
+  public void testClone() {
+    PassengerVehicle pV1 = new PassengerVehicle("Bob", 3, 1);
+    PassengerVehicle pV2 = pV1.clone();
+    
+    assertEquals(pV1.getSeatNum(), pV2.getSeatNum());
+  }
+}
