ba0be0cf238b5599e867933a9b3140c06b80d02f
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2012-05-01 17:14:31 +0900

    add ex04_02

---
+++ JPL/ch04/ex04_02/SortHarnessTest.java
@@ -0,0 +1,30 @@
+package ch04.ex04_02;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SortHarnessTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void testSortArray1() {
+    String[] testData = new String[3];
+    testData[0] = "Yamada Taro";
+    testData[1] = "Yamada"; 
+    testData[2] = "Yamada Hanako";
+        
+    SortHarness<String> sHarness = new SortString(testData);
+    sHarness.sort();
+    String[] expected = new String[3];
+    expected[0] = "Yamada";
+    expected[1] = "Yamada Taro";
+    expected[2] = "Yamada Hanako";
+    
+    assertArrayEquals(expected, testData);
+  }
+}
