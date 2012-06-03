fb9070771d18e9111d5dd268b89b8ea6ec27785c
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2012-06-04 08:48:15 +0900

    fix source

--- JPL/ch01/ex01_02/HelloWorld.java
+++ JPL/ch01/ex01_02/HelloWorld.java
@@ -2,7 +2,7 @@ package ch01.ex01_02;
 
 class HelloWorld {
   public static void main(String[] args) {
-    System.out.print
-    ln("Hello, world");
+    //System.out.print
+    //ln("Hello, world");
   }
 }
