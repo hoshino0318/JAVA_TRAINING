15f29db3a01a8a9671d293d326f508acf8d7c626
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2012-06-10 21:38:39 +0900

    add ex11_03

---
+++ JPL/ch11/ex11_03/AttributedImpl.java
@@ -0,0 +1,35 @@
+package ch11.ex11_03;
+
+import java.util.*;
+import java.util.Iterator;
+
+class AttributedImpl<T> implements Attributed<T>, Iterable<Attr<T>> {
+  protected Map<String, Attr<T>> attrTable =
+      new HashMap<String, Attr<T>>();
+  
+  @Override
+  public void add(Attr<T> newAttr) {
+    attrTable.put(newAttr.getName(), newAttr);
+  }
+
+  @Override
+  public Attr<T> find(String name) {
+    return attrTable.get(name);
+  }
+
+  @Override
+  public Attr<T> remove(String name) {
+    return attrTable.remove(name);
+  }
+
+  @Override
+  public Iterator<Attr<T>> attrs() {
+    return attrTable.values().iterator();
+  }
+  
+  @Override
+  public Iterator<Attr<T>> iterator() {
+    return attrs();
+  }
+
+}
