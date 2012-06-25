de58d90174a7fd6a42cbd4797aa54b56d09d8b4f
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2012-06-25 22:25:07 +0900

    add ex13_01

---
+++ JPL/ch13/ex13_01/MyStringTest.java
@@ -0,0 +1,37 @@
+package ch13.ex13_01;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyStringTest {
+
+  @Test
+  public void testCount1() {
+    MyString myStr = new MyString("hogehoge");
+    int expectedCount = 2;
+    char targetChar = 'h';
+    
+    assertEquals(expectedCount, myStr.count(targetChar));
+  }
+  
+  @Test
+  public void testCount2() {
+    MyString myStr = new MyString("hogehoge");
+    int expectedCount = 0;
+    char targetChar = 'y';
+    
+    assertEquals(expectedCount, myStr.count(targetChar));
+  }
+  
+  @Test
+  public void testCount3() {
+    MyString myStr = new MyString("122333444455555");
+    int expectedCount = 3;
+    char targetChar = '3';
+    
+    assertEquals(expectedCount, myStr.count(targetChar));
+  }
+  
+}
