752d24342f64a84b60cd6433291bab8f28ce7e8f
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2012-08-29 21:30:01 +0900

    add Concat.java

---
+++ sandbox/Concat.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+class Concat {
+  public static void main(String[] args)
+    throws IOException
+  {
+    InputStream in; // 文字を読み込むべきストリーム
+    if (args.length == 0) {
+      in = System.in;
+    } else {
+      InputStream fileIn, bufIn;
+      List<InputStream> inputs =
+          new ArrayList<InputStream>(args.length);
+      for (String arg : args) {
+        fileIn = new FileInputStream(arg);
+        bufIn = new BufferedInputStream(fileIn);
+        inputs.add(bufIn);
+      }
+      Enumeration<InputStream> files =
+          Collections.enumeration(inputs);
+      in = new SequenceInputStream(files);
+    }
+    int ch;
+    while ((ch = in.read()) != -1)
+      System.out.write(ch);
+  }
+}
