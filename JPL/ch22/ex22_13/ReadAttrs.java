0f4eb9c0bbb309dabf9a60a31d0d8ff5e81616b5
HOSHINO Tatsuya <tatsuya.hoshino@nts.ricoh.co.jp>
2012-10-01 23:52:55 +0900

    add ex22_13 (finished)

--- JPL/ch22/ex22_13/ReadAttrs.java
+++ JPL/ch22/ex22_13/ReadAttrs.java
@@ -6,42 +6,30 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Iterator;
-import java.util.regex.*;
 import java.util.Scanner;
 
 class ReadAttrs {
-  public static Attributed readAttrs(Reader source) 
+  public static Attributed readAttrs(Reader source)
       throws IOException {
     Scanner in = new Scanner(source);
-    in.useDelimiter("=|\\s+");
+    in.useDelimiter("\\s+|=]");
     AttributedImpl attrs = new AttributedImpl();
-    String exp = "(\\S+)";
-    Pattern pat = Pattern.compile(exp);
     Attr attr = null;
     while (in.hasNext()) {
-      if (in.hasNext(pat)) {
-        String next = in.next(pat);
-        if (next.equals("=")) {
-          if (attr == null)
-            throw new IOException("misplaced '='");
-          
-          
-            
-        } else {
-          if (attr == null) {
-            attr = new Attr(next);
-            attrs.add(attr);
-            in.useDelimiter("\\s+");
-          } else {
-            attr.setValue(next);
-            attr = null;
-          }
-        }
+      String next = in.next();
+      if (attr == null) {
+        if (next.equals(""))
+          throw new IOException("bad format");
+        attr = new Attr(next);
+        attrs.add(attr);
+        in.useDelimiter("\\s*");
       } else {
-        System.out.println("debug");
-        String d = in.next();
-        if (d.equals("=") && attr == null) {
-          throw new IOException("misplaced '='");
+        if (next.equals("=")) {
+          in.useDelimiter("\\s+");
+        } else {
+          attr.setValue(next);
+          attr = null;
+          in.useDelimiter("\\s+|=");
         }
       }
     }
@@ -54,9 +42,9 @@ class ReadAttrs {
       System.err.println("needs input files");
       System.exit(1);
     }
-    
+
     for (String fpath : args) {
-      BufferedReader in = null;      
+      BufferedReader in = null;
       try {
         in = new BufferedReader(new FileReader(fpath));
         Attributed attrs = readAttrs(in);
